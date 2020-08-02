dd346c7188879a5d6b4f5e227f6b30ca754f960e
ditrim023 <krutoguz@cloudwharf.com>
2020-08-02 20:52:20 +0300

    header in mail service work

--- src/main/java/com/example/mail/controller/PageController.java
+++ src/main/java/com/example/mail/controller/PageController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
 
+    @GetMapping("/")
+    public String mails() {
+        return "mails";
+    }
 }
