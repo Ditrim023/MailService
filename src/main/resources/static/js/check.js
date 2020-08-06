3f5cbb90136ddb4aaee7fc4fb00780150e11fc5f
ditrim023 <krutoguz@cloudwharf.com>
2020-08-06 13:59:38 +0300

    add validator to new list

---
+++ src/main/resources/static/js/check.js
@@ -0,0 +1,39 @@
+$( "#myinput" ).rules( "add", {
+    minlength: 2
+});
+
+$('#mail-form').validate({
+    rules: {
+        receivers: {
+            required: true,
+        },
+        theme: {
+            required: true,
+            maxlength: 150
+        },
+        text: {
+            required: true,
+            maxlength: 1024
+        },
+    },
+    messages: {
+        receivers: {
+            required: 'Поле не должно быть пустым'
+        },
+        theme: {
+            required: 'Поле не должно быть пустым',
+            maxlength: 'Максимум 150 символов'
+        },
+        text: {
+            required: 'Поле не должно быть пустым',
+            maxlength: 'Максимум 1024 символа'
+        }
+    },
+    errorPlacement: function (error, element) {
+        if (element.attr('type') == 'radio') {
+            error.insertBefore(element.parent());
+        } else {
+            error.insertBefore(element);
+        }
+    }
+});
